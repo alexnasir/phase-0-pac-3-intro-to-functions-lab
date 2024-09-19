604ad590588d965a0a3a6168209ae6d49d92c682
alex <alexnasiali45@gmail.com>
2024-09-19 11:52:12 +0300

    new

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(string){
+    return string.toUpperCase();
+}
+function whisper(HELLO){
+    return HELLO.toLowerCase();
+}
+logShout(STRING)
+ console.logShout
+ logWhisper(string)
+ console.logWhisper
+
